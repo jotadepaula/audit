265c5132eb3cbd560776b22bebba8b823d8f50ed
jotadepaula <jotaskt@gmail.com>
2013-11-20 01:50:26 -0200

    export to cvs

--- audit.sh
+++ audit.sh
@@ -2,17 +2,17 @@
 hostScan="127.0.0.1"
 function exportPermissoes () { 
 	listPer=$(ls -l)
-	echo "$listPer" >> listPer.log
+	echo "$listPer" >> listPer.cvs
 
 }
  function scan () {
  	listScan=$(nmap -v $hostScan)
- 	echo "$listScan" >> listScan.log
+ 	echo "$listScan" >> listScan.cvs
  }
 
  function verCompartilhamentos() {
  	samba=$(smbclient -L 127.0.0.1 -N)
- 	echo "$samba" >> verCompartilhamentos.log
+ 	echo "$samba" >> verCompartilhamentos.cvs
  }
 exportPermissoes
 scan
