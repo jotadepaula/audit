5d86dd868302ee3f80b084c5ff584fdebc685752
jotadepaula <jotaskt@gmail.com>
2013-12-04 19:23:40 -0200

    gran Finale

--- audit.sh
+++ audit.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
 function exportPermissoes () { 
-	listPer=$(ls -l)
+	listPer=$(ls -l $1)
+	echo -e "Exportando permissoes..."
 	echo "$listPer" >> listPer.cvs
+	echo -e "Permissoes exportadas com Sucesso"
 
 }
  function scan () {
 	listScan=$(nmap -v $1)
- 	echo -n "escaneado IP ..."
- 	echo -n "Exportando resultados ..."
- 	echo -n "$listScan" >> listScan.cvs
- 	echo "Escaneamento conluido com sucesso"
+ 	echo -e "escaneado IP ... "
+ 	echo -e "Exportando resultados ..."
+ 	echo -e "$listScan" >> listScan.cvs
+ 	echo -e "Escaneamento conluido com sucesso "
  }
 
  function verCompartilhamentos() {
- 	samba=$(smbclient -L 127.0.0.1 -N)
+  	echo -e " verificando Compartilhamentos..."
+  	echo -e "Exportando resultados ..."
+ 	samba=$(smbclient -L $1 -N)
  	echo "$samba" >> verCompartilhamentos.cvs
+ 	echo -e "Verifcação conluida com sucesso "
  }
-echo -n "digite o IP a ser escaneado:"
-read $hostScan
+ 
+echo -e "digite o IP a ser escaneado:"
+read hostScan
 scan "$hostScan"
+
+echo -e "Verificar permissoes dos diretórios"
+echo -e "Digite o diretório a ser verificado"
+read dir
+exportPermissoes "$dir"
+
+echo -e "Verificar Compartilhamentos na Rede"
+echo -e "Digite o IP para verificar os Compartilhamentos"
+read hostShare
+verCompartilhamentos "$hostShare"
 
