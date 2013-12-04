31fd882d39628d3ee5a44325ff90ee37efbb7ab5
jotadepaula <jotaskt@gmail.com>
2013-12-04 03:03:35 -0200

    quase acabando essa merda

--- audit.sh
+++ audit.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-hostScan="127.0.0.1"
+
 function exportPermissoes () { 
 	listPer=$(ls -l)
 	echo "$listPer" >> listPer.cvs
 
 }
  function scan () {
- 	listScan=$(nmap -v $hostScan)
- 	echo "$listScan" >> listScan.cvs
+	listScan=$(nmap -v $1)
+ 	echo -n "escaneado IP ..."
+ 	echo -n "Exportando resultados ..."
+ 	echo -n "$listScan" >> listScan.cvs
+ 	echo "Escaneamento conluido com sucesso"
  }
 
  function verCompartilhamentos() {
  	samba=$(smbclient -L 127.0.0.1 -N)
  	echo "$samba" >> verCompartilhamentos.cvs
  }
-exportPermissoes
-scan
-verCompartilhamentos
+echo -n "digite o IP a ser escaneado:"
+read $hostScan
+scan "$hostScan"
+
